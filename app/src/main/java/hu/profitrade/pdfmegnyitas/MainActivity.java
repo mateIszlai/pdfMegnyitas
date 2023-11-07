fb4ad7cd0acb72e6b739be6b4e6aab3df6f2b03f
Mate Iszlai <iszimate@gmail.com>
2023-11-07 08:42:10 +0100

    Open pdf

--- app/src/main/java/hu/profitrade/pdfmegnyitas/MainActivity.java
+++ app/src/main/java/hu/profitrade/pdfmegnyitas/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.DocumentsContract;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 import hu.profitrade.pdfmegnyitas.databinding.ActivityMainBinding;
 
@@ -45,6 +52,28 @@ public class MainActivity extends AppCompatActivity {
                 uri = data.getData();
 
             binding.pathTv.setText(uri == null ? "" : uri.getPath());
+
+            if(uri != null){
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(uri, "application/pdf");
+                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException ignored){
+                    StringBuilder sb = new StringBuilder();
+                    try (InputStream inputStream =
+                            getContentResolver().openInputStream(uri);
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(Objects.requireNonNull(inputStream)))){
+                          String line;
+                          while((line = reader.readLine()) != null)
+                              sb.append(line);
+                        } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
         }
     }
 }
